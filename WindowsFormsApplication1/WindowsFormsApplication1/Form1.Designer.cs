58b7af54e330eda8edb60a762c13420f6a30e478
Yusuf Gurdag <ysfgurdag@gmail.com>
2016-06-19 14:13:10 +0300

    uc

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(173, 107);
+            this.button1.Location = new System.Drawing.Point(168, 203);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
